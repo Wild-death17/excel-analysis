6af3ecc8a43fbf21f76e4ebd9bb3312165cd6a7d
Moshe <@Wild_death17>
2024-02-11 18:03:05 +0200

    date and time redone on file name

--- middleware/uploads_mw.js
+++ middleware/uploads_mw.js
@@ -9,18 +9,21 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/');
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now()+ '-' + file.originalname);
+        let dateNtimeStr = `${new Date().toLocaleDateString().replace(/\//g, '.')}`;
+        dateNtimeStr += ` ${new Date().toLocaleTimeString().replace(/:/g, '-')}`;
+        cb(null, dateNtimeStr + file.originalname);
     }
 });
 
-const upload = multer({ storage : storage });
-async function file_con (req,res,next){
-    if(!req.file)
+const upload = multer({storage: storage});
+
+async function file_con(req, res, next) {
+    if (!req.file)
         return res.status(400).json({error: 'No File Uploaded.'})
     const filepate = req.file.path;
     console.log(req.file.originalname)
     console.log(filepate)
-    if(!filepate.endsWith('.xlsx')){
+    if (!filepate.endsWith('.xlsx')) {
         fs.unlinkSync(filepate);
         return res.status(400).json({error: 'Invalid File Format.'});
     }
@@ -33,9 +36,9 @@ async function file_con (req,res,next){
 }
 
 module.exports = {
-    fs:fs,
-    xlsx:xlsx,
-    multer:multer,
-    upload:upload,
-    file_con:file_con
+    fs: fs,
+    xlsx: xlsx,
+    multer: multer,
+    upload: upload,
+    file_con: file_con
 }
