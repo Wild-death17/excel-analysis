21d6e77d3a380dc90a3df7057e90eb74cb5d132a
Moshe <Moshe150519@Gmail.com>
2024-05-09 18:44:01 +0300

    Renamed : EndPoints, MiddleWare functions.

--- Route/Data-Ext-Route.js
+++ Route/Data-Ext-Route.js
@@ -4,6 +4,9 @@ const DataExtMW = require('../MiddleWare/Data-Ext-MW');
 const Route = express.Router();
 module.exports = Route;
 
-Route.post('/get_Points', [FileMW.ReadFile, DataExtMW.get_Data_json, DataExtMW.get_Points], (req, res) => {
-    res.status(200).json(res.Points)
+Route.post('/Points', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Extract_XY_Points], (req, res) => {
+    res.status(200).json(req.Points)
+});
+Route.post('/Slopes', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Calculate_Gas_Slopes], (req, res) => {
+    res.status(200).json(req.Slopes)
 });
