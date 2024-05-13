3724474cd8c557e4170142fb846e3d4dfca5d75b
MichaelChemo2507 <mnh8551@gmail.com>
2024-05-13 20:17:38 +0300

    Added a gases btn

--- js/MainPage.js
+++ js/MainPage.js
@@ -1,23 +1,27 @@
 let NavBox = document.getElementById("NavBox"),
-    NavList = document.getElementById("NavList");
+    NavList = document.getElementById("NavList"),
+    GasList = document.getElementById("GasesList");
+let GasesArr = [`<div class="item">mkdj"</div>`,`<div class="item">"kdjd"</div>`,`<div class="item">"ldjdu</div>`];
 let FileStr = [`<input class="item" type="file" name="file" id="UploadFile" onchange="FileMethods.Upload()">`, `<div onclick="FileMethods.Read()" class="item"> Read Files</div>`, `<div onclick="FileMethods.Delete()" class="item"> Delete File </div>`];
 let FileMethods = {
     Upload: async function () {
         let fileVal = document.getElementById("UploadFile");
         const formData = new FormData();
         formData.append('file', fileVal.files[0]);
-        let res = await fetch("http://localhost:2507/Files/UploadFile"
+        await fetch("http://localhost:2507/Files/UploadFile"
             , {
                 method: 'POST',
                 body: formData
             })
-        alert(res.send)
+        alert("File uploaded successfully");
     }, Read: function () {
+
     }, Delete: function () {
     }
 };
 
 CreateList(FileStr, NavList);
+CreateList(GasesArr, GasList);
 
 function CreateList(ListArr, List) {
     let str = "";
@@ -29,10 +33,5 @@ function CreateList(ListArr, List) {
 
 function ToggleList(elmId) {
     let elm = document.getElementById(elmId);
-    if (elm.style.display === "grid") {
-        elm.style.display = "none";
-    } else {
-        elm.style.display = "grid";
-
-    }
+    elm.style.display = (elm.style.display === "grid")?"none": "grid";
 }
