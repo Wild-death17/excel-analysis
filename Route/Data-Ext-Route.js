59680a7a2885f409a0551d796a43bfce03aa5649
Moshe <Moshe150519@Gmail.com>
2024-05-16 19:11:08 +0300

    Refactor : Var Names, Changed : EndPoints (post -> get).

--- Route/Data-Ext-Route.js
+++ Route/Data-Ext-Route.js
@@ -4,9 +4,9 @@ const DataExtMW = require('../MiddleWare/Data-Ext-MW');
 const Route = express.Router();
 module.exports = Route;
 
-Route.post('/Points', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Extract_XY_Points], (req, res) => {
+Route.get('/Points', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Extract_XY_Points], (req, res) => {
     res.status(200).json(req.Points)
 });
-Route.post('/Slopes', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Calculate_Gas_Slopes], (req, res) => {
+Route.get('/Slopes', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Calculate_Gas_Slopes], (req, res) => {
     res.status(200).json(req.Slopes)
 });
