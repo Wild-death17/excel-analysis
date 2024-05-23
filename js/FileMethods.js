cdb34554849b53699c68b518bfbdbd98f4180fca
MichaelChemo2507 <mnh8551@gmail.com>
2024-05-23 20:40:16 +0300

    Start working on the new design - Finish working on the upload saction

---
+++ js/FileMethods.js
@@ -0,0 +1,79 @@
+
+let FileMethods = {
+    Upload: async function () {
+        let fileVal = document.getElementById("inputFile");
+        const formData = new FormData();
+        formData.append('file', fileVal.files[0]);
+        await fetch("/Files/UploadFile"
+            , {
+                method: 'POST',
+                body: formData
+            })
+        alert("File uploaded successfully");
+        FileMethods.addFileToDeck();
+    },
+    addFileToDeck(){
+
+    },
+    Read: async function () {
+        return await FileMethods.FetchFilesPath("/Files/GetFile");
+    },
+    ExtractData: async function (FilePath){
+        let GasesStr = [];
+        let Response = await fetch("/DataText/Points",{
+            method:"POST",
+            headers: {
+                'content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                FilePath: FilePath
+            })
+        })
+        Response = await Response.json();
+        Points = Response;
+        for (const dataKey in Response) {
+            GasesStr.push(`<option class="item" value="${dataKey}">${dataKey}</option>`);
+        }
+        CreateList(GasesStr, GasList);
+    },
+    Display:async function () {
+        let data = await FileMethods.Read();
+        let str = "";
+        for (const path of data) {
+            str += `<div onclick="FileMethods.ExtractData('${path}')" class="item">${path}</div>`;
+        }
+        ReadList.innerHTML = str;
+        ToggleList("ReadList");
+    },
+    Delete: async function () {
+        let data = await FileMethods.Read();
+        let str = "";
+        console.log(data)
+        for (const path of data) {
+            str += `<div onclick="FileMethods.DeleteFile('${path}')" class="item">${path}</div>`;
+        }
+        DeleteList.innerHTML = str;
+        ToggleList("DeleteList");
+    },
+    DeleteFile : async function (FilePath) {
+        if (confirm("do you wont to delete this file?")) {
+            await fetch("http://localhost:2507/Files/DeleteFile", {
+                method: "DELETE",
+                headers: {
+                    'content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    filePath: FilePath
+                })
+            })
+        }
+        await FileMethods.Delete();
+    },
+    FetchFilesPath:async function (urlLocation){
+        let response = await fetch(urlLocation, {
+            method: "GET"
+        })
+        let data = await response.json();
+        return data;
+    }
+};
