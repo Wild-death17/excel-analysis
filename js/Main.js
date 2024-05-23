e7a9eee9ec7ac03d087e16e8eaa4d861a41107dc
MichaelChemo2507 <mnh8551@gmail.com>
2024-05-23 21:20:56 +0300

    Finish working on the upload section - start working on the files section

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,14 @@
-
-
+let DropArea = document.getElementById("dropArea"),
+    InputFile = document.getElementById("inputFile");
 let NavListStr = [`<input class="item" type="file" name="file" id="UploadFile" onchange="FileMethods.Upload()">`, `<div onclick="FileMethods.Display()" class="item"> Read Files</div>`, `<div onclick="FileMethods.Delete()" class="item"> Delete File </div>`];
 let Points = [];
+
+DropArea.addEventListener("dragover", function (e) {
+    e.preventDefault();
+})
+DropArea.addEventListener("drop", async function (e) {
+    e.preventDefault();
+    InputFile.files = e.dataTransfer.files;
+    await FileMethods.Upload();
+})
 
