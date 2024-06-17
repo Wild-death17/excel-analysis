57d9e753597cfc0b9f29ef17baee5679d80bc646
Moshe <Moshe150519@Gmail.com>
2024-06-17 17:40:38 +0100

    Added: Route - GasNames retrieves the Names of the gasses in Excel

--- Route/Data-Ext-Route.js
+++ Route/Data-Ext-Route.js
@@ -4,16 +4,20 @@ const DataExtMW = require('../MiddleWare/Data-Ext-MW');
 const Route = express.Router();
 module.exports = Route;
 
+let ReadFileObject = [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet];
 
-Route.post('/Points', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Extract_XY_Points], (req, res) => {
+Route.post('/Points', [ReadFileObject, DataExtMW.Extract_XY_Points], (req, res) => {
     res.status(200).json(req.Points);
 });
-Route.post('/Slopes', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Calculate_Gas_Slopes], (req, res) => {
+Route.post('/Slopes', [ReadFileObject, DataExtMW.Calculate_Gas_Slopes], (req, res) => {
     res.status(200).json(req.Slopes)
 });
-Route.post('/ChartSeries', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Extract_XY_Points, DataExtMW.Get_Chart_Series], (req, res) => {
+Route.post('/GasNames', [ReadFileObject, DataExtMW.Get_Gas_Names], (req, res) => {
+    res.status(200).json(req.Names)
+});
+Route.post('/ChartSeries', [ReadFileObject, DataExtMW.Extract_XY_Points, DataExtMW.Get_Chart_Series], (req, res) => {
     res.status(200).json(req.Series)
 });
-Route.post('/ExpStartEnd', [FileMW.ReadFile, DataExtMW.Extract_Object_From_Exel_Sheet, DataExtMW.Get_Exp_Start_End], (req, res) => {
+Route.post('/ExpStartEnd', [ReadFileObject, DataExtMW.Get_Exp_Start_End], (req, res) => {
     res.status(200).json(req.StartEnd)
 });
