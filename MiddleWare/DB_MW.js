907884a67f5037594279c85e95eba5103d08db9f
MichaelChemo2507 <mnh8551@gmail.com>
2024-06-18 13:29:20 +0300

    Start working on the DB MW functions.(CRUD)

---
+++ MiddleWare/DB_MW.js
@@ -0,0 +1,23 @@
+function Read_All_Rows() {
+
+}
+function Read_Row() {
+
+}
+function Add_Row() {
+
+}
+function Update_Row() {
+
+}
+function Delete_Row() {
+
+}
+
+module.exports = {
+    Read_All_Rows:Read_All_Rows,
+    Read_Row:Read_Row,
+    Add_Row:Add_Row,
+    Update_Row:Update_Row,
+    Delete_Row:Delete_Row
+}
