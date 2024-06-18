3da7f2795e4806bd2523d789b99b34d0613abb8c
MichaelChemo2507 <mnh8551@gmail.com>
2024-06-18 20:21:14 +0300

    Changed the methods flow for secure purposes, now the Read and Delete is global and the Add and Update is in a different mw file specific to the working table.

--- MiddleWare/DB_MW.js
+++ MiddleWare/DB_MW.js
@@ -23,46 +23,22 @@ function Read_Row(req, res, next) {
     next();
 }
 
-function Add_Row(req, res, next) {
-    let {Table_Name} = req.body;
-    let Columns = [];
-    let Values_Str = "";
-    let Columns_Name_Str = "";
-    for (const columnName in res.body.columnName) {
-        Columns.push(columnName);
-    }
-    let Query = `INSERT INTO  ${Table_Name}(`;
-    for (let i = 0; i < Columns.length; i++) {
-        Columns_Name_Str += `${Columns[i]}`;
-        Values_Str += `${req.body.Columns[i]}`;
-        if (i < Columns.length - 1)
-            Query += ",";
-        else
-            Query += ") ";
-    }
-    Query += "VALUES(";
-
-    db_pool.query(q, function (err, rows, fields) {
-        if (err) {
-            res.status(500).json({message: err});
-        } else {
-            res.redirect('/front/bg');
-        }
-    })
-}
-
-function Update_Row(req, res, next) {
-
-}
-
 function Delete_Row(req, res, next) {
+    let {Table_Name, Column_Name} = req.body;
+    let {Id} = req.params;
+    let Query = `DELETE * FROM ${Table_Name} WHERE ${Column_Name} = ${Id}`;
+    db_pool.query(Query, (err, row, fields) => {
+        if (err) {
+            return res.status(500).json({message: err});
+        }
+        res.status(200).json({RowDeleted: row.insertId});
 
+    })
+    next();
 }
 
 module.exports = {
     Read_All_Rows: Read_All_Rows,
     Read_Row: Read_Row,
-    Add_Row: Add_Row,
-    Update_Row: Update_Row,
     Delete_Row: Delete_Row
 }
